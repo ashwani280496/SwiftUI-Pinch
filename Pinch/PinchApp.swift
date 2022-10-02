8ad888faf62aeb44ec0e81442d91dd512533078b
Ashwani Shakya <ashwani.kumar@theporter.in>
2022-10-02 11:36:55 +0530

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Ashwani Kumar on 02/10/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
